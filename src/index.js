d24a9ff2a27c5548407c1f0d6bb994fdca19c8a3
Ayush Indoria <96787650+akkii03@users.noreply.github.com>
2022-08-14 23:19:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
 var http = require("http");
-
 const httpServer = http.createServer(handleServer);
 const port = 8081;
-
 
 function handleServer(req, res) {
   if(req.url==='/welcome'){
     res.statusCode=200;
-    res.end("Welcome to Dominos!");
+    return res.end("Welcome to Dominos!");
   }
-
   if(req.url==='/contact') {
     res.statusCode=200;
-    res.end(JSON.stringify(
+    return res.end(JSON.stringify(
         {phone: '18602100000',
     email: 'guestcaredominos@jublfood.com'}
-    ))
-    
+    ))  
   }
   else{
     res.statusCode=404;
-    res.end("404");
+   return  res.end("404");
   }
 }
-
 httpServer.listen(port);
-
 module.exports = httpServer;
