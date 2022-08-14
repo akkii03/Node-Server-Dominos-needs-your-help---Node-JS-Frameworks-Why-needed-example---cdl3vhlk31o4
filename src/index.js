570c7de1a7849150a6bcfcf3266f165f977f6389
Ayush Indoria <96787650+akkii03@users.noreply.github.com>
2022-08-14 20:15:59 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 var http = require("http");
 
 const httpServer = http.createServer(handleServer);
+const port = 8081;
 
 
 function handleServer(req, res) {
-  
+  if(req.url==='/welcome'){
+    res.end("Welcome to Dominos!");
+    res.sendStatus(200);
+  }
+
+  if(req.url==='/contact') {
+    res.end(JSON.stringify(
+        {phone: '18602100000',
+    email: 'guestcaredominos@jublfood.com'}
+    ))
+  }
+  else{
+    res.sendStatus(404);
+  }
 }
+
+httpServer.listen(port);
 
 module.exports = httpServer;
